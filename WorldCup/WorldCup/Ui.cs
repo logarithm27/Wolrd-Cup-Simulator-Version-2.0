1c07313d89f774cfe047b9c704943e1ccbca9c60
Maftoul Omar <maftoul.omar@gmail.com>
2019-01-20 20:25:45 +0100

    Comments

--- WorldCup/WorldCup/Ui.cs
+++ WorldCup/WorldCup/Ui.cs
@@ -21,17 +21,25 @@ namespace WorldCup
                 ref canvas
             );
         }
-      
+         //Initializing rectangles in each pot
+        //Left and Top are margins relatives to the canvas (Rectangles are inside Canvas)
         private void InitRectanglesOnPots(double left,double top,ref Canvas canvas)
         {
+            //starting from the highest Top margin value until it reach a negative value
+            //we've Initialized all rectangles of the previous pot
             if (top<0)
             {
+                //we start again until we have a negative value on left margin
                 left -= (double)UiParameters.CanvasPotsParameters.AddByOnCanvasLeft;
+                //Initializing rectangles on a new pot
                 top = (double)UiParameters.CanvasPotsParameters.CanvasMaxTop;
             }
+            //We've done with all rectangles of all pots
             if (left < 0)
                 return;
+            //Add each rectangle to Canvas
             AddRectangleOnCanvas(left, top, ref canvas);
+            //Draw next rectangle on the same pot
             top -= (double)UiParameters.CanvasPotsParameters.AddByOnCanvasTop;
             InitRectanglesOnPots(left, top, ref canvas);
         }
@@ -43,6 +51,7 @@ namespace WorldCup
             Canvas.SetTop(rect, top);
             Canvas.SetLeft(rect, left);
             canvas.Children.Add(rect);
+            //add rectangles to List of rectangles
             Rectangles.Add(rect);
         }
         private Rectangle CreateRectangle()
--- WorldCup/WorldCup/UiParameters.cs
+++ WorldCup/WorldCup/UiParameters.cs
@@ -1,19 +1,31 @@
-﻿namespace WorldCup
+﻿/*All components (rectangles on pots/groups) are relative to canvas (it's important to remember).
+        *VALUES ARE CONSISTENT with :
+        *         -The graphical visualization
+        *         -Number of rectangles to draw
+        *before changing these values, make sure having a nice UI to see,
+        *and do the calculations that will varies on how much teams you want to have
+        *i.e : i draw a rectangle that have 282 (as max top value),
+        * and I draw another by subtracting -40 from 282 until I reach 0, hence 282/40 ~=7 (I need 8 Teams)
+        *Each pot holds 8 rectangles (teams), 
+*/
+namespace WorldCup
 {
     public  struct UiParameters
     {
+        //Parameters of rectangles ( rectangles will holds flag of national team)
         public  enum ShapeParameters
         {
             Width = 40,
             Height = 20,
             StrokeThickness = 1,
         }
-
+       
         public enum CanvasPotsParameters
         {
-            CanvasMaxTop = 282,
+
+            CanvasMaxTop = 282, // Maximum TOP value to not exceed  
             CanvasMaxLeft = 490,
-            AddByOnCanvasTop = 40,
+            AddByOnCanvasTop = 40, // i.e : The first Rectangle has a 282 Top Value, hence the next rectangle will have (282-40) on Top Value and etc..
             AddByOnCanvasLeft = 160,
         }
 
