672be5feca2e49f1c03b92e2b32487b1a4c48b31
Maftoul Omar <maftoul.omar@gmail.com>
2019-01-26 21:11:47 +0100

    commit

--- WorldCup/WorldCup/MainWorldCupWindow.xaml.cs
+++ WorldCup/WorldCup/MainWorldCupWindow.xaml.cs
@@ -1,8 +1,5 @@
-﻿using System;
-using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Media;
-using System.Windows.Media.Animation;
+﻿using System.Windows;
+
 
 namespace WorldCup
 {
@@ -25,29 +22,28 @@ namespace WorldCup
         private void DrawButtonClick(object sender, RoutedEventArgs e)
         {
 
-            var top = Canvas.GetTop(_userInterface.Rectangles[31]);
-            var left = Canvas.GetLeft(_userInterface.Rectangles[31]);
-            //Translates (moves) an object in the 2-D x-y coordinate system.
-            TranslateTransform trans = new TranslateTransform();
-            _userInterface.Rectangles[31].RenderTransform = trans;
-            //creates a transition between two double values (From-->To)
-            DoubleAnimation topAnimation =
-                new DoubleAnimation(
-                    top,
-                    (double) UiParameters.CanvasGroupsParameters.CanvasMinTop - top,
-                    TimeSpan.FromSeconds(2.3)
-                );
-            DoubleAnimation leftAnimation =
-                new DoubleAnimation(
-                    left,
-                    (double) UiParameters.CanvasGroupsParameters.CanvasMinLeft
-                    + (double) UiParameters.CanvasGroupsParameters.AddByOnCanvasLeft - left,
-                    TimeSpan.FromSeconds(2.3)
-                );
-            // BeginAnimation Starts an animation for a specified animated property on this element.
-            trans.BeginAnimation(TranslateTransform.YProperty, topAnimation);
-            trans.BeginAnimation(TranslateTransform.XProperty, leftAnimation);
-            //MessageBox.Show(Canvas.GetTop(_userInterface.ConfederationsImages.First().Value);
+            //var top = Canvas.GetTop(_userInterface.Rectangles[31]);
+            //var left = Canvas.GetLeft(_userInterface.Rectangles[31]);
+            ////Translates (moves) an object in the 2-D x-y coordinate system.
+            //TranslateTransform trans = new TranslateTransform();
+            //_userInterface.Rectangles[31].RenderTransform = trans;
+            ////creates a transition between two double values (From-->To)
+            //DoubleAnimation topAnimation =
+            //    new DoubleAnimation(
+            //        top,
+            //        (double) UiParameters.CanvasGroupsParameters.CanvasMinTop - top,
+            //        TimeSpan.FromSeconds(2.3)
+            //    );
+            //DoubleAnimation leftAnimation =
+            //    new DoubleAnimation(
+            //        left,
+            //        (double) UiParameters.CanvasGroupsParameters.CanvasMinLeft
+            //        + (double) UiParameters.CanvasGroupsParameters.AddByOnCanvasLeft - left,
+            //        TimeSpan.FromSeconds(2.3)
+            //    );
+            //// BeginAnimation Starts an animation for a specified animated property on this element.
+            //trans.BeginAnimation(TranslateTransform.YProperty, topAnimation);
+            //trans.BeginAnimation(TranslateTransform.XProperty, leftAnimation);
         }
     }
 }
