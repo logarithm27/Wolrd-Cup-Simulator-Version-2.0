289ea0164ee5d0cfd95a25b378bf8943acdcc17a
Maftoul Omar <maftoul.omar@gmail.com>
2019-01-26 21:41:58 +0100

    Search by key and value on dictionnaries by using linq

---
+++ WorldCup/WorldCup/Utility.cs
@@ -0,0 +1,24 @@
+﻿/*Maftoul Omar 2019*/
+
+using System.Collections.Generic;
+using System.Linq;
+
+namespace WorldCup
+{
+        public  static class  Utility
+        {
+        public static object DictionarySearchByKey(Dictionary<object, object> collection, object obj)
+            {
+                if (collection.ContainsKey(obj))
+                    return collection.FirstOrDefault(i => i.Key.Equals(obj));
+                throw new System.NotImplementedException();
+            }
+
+            public static object DictionarySearchByValue(Dictionary<object, object> collection, object obj)
+            {
+                if (collection.ContainsValue(obj))
+                    return collection.FirstOrDefault(i => i.Value.Equals(obj));
+                throw new System.NotImplementedException();
+            }
+    }
+}
