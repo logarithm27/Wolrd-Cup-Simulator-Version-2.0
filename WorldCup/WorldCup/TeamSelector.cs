f68c4327902ef3f24486ead43dd10730efbff798
Maftoul Omar <maftoul.omar@gmail.com>
2019-01-28 22:02:08 +0100

    commit

--- WorldCup/WorldCup/TeamSelector.cs
+++ WorldCup/WorldCup/TeamSelector.cs
@@ -1,9 +1,16 @@
 ﻿using System.Windows;
+using System.Windows.Controls;
 
 namespace WorldCup
 {
     public class TeamSelector : UIElement
     {
+        private ListBox _teamList;
+        public ListBox TeamList {get => _teamList;set => _teamList=value;}
+        private Label _teamName;
+        public Label TeamName { get => _teamName; set => _teamName = value; }
+        private Canvas _canvas;
+
 
     }
 }
