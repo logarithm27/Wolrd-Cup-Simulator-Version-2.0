097d5b60280fc8c2fa41bb10e96a7e835d28e386
Maftoul Omar <maftoul.omar@gmail.com>
2019-01-29 22:54:31 +0100

    commit

--- WorldCup/WorldCup/Ui.cs
+++ WorldCup/WorldCup/Ui.cs
@@ -129,28 +129,7 @@ namespace WorldCup
         
         private void OnMouseClickImage(object sender, MouseEventArgs e)
         {
-            _teamSelector.Items.Clear();
-            _teamSelector.Items.Insert(0, Utility.GetKeyByValue(_confederationsImages, sender));
-           
-            for (int i = 1; i < 80; i++)
-            {
-                var confederation = "test";
-                Label label = new Label();
-                label.Content = confederation;
-                Canvas canvas = new Canvas { Width = 45, Height = 25 };
-                var rectangle = new Rectangle
-                {
-                    Width = 40,
-                    Height = 20,
-                    Stroke = Brushes.Black,
-                };
-                Canvas.SetTop(rectangle, 3);
-                Canvas.SetLeft(rectangle, -1);
-                Canvas.SetLeft(label, Canvas.GetLeft(rectangle) + 40);
-                canvas.Children.Add(rectangle);
-                canvas.Children.Add(label);
-                _teamSelector.Items.Insert(i, canvas);
-            }
+       
 
             
         }
