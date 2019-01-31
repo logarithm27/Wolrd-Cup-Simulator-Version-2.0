5ac80085ea2b5773be61b3ef0f1cac2e97dd4344
Maftoul Omar <maftoul.omar@gmail.com>
2019-01-31 19:48:59 +0100

    commit

--- WorldCup/WorldCup/TeamSelector.cs
+++ WorldCup/WorldCup/TeamSelector.cs
@@ -8,39 +8,42 @@ namespace WorldCup
     {
         private ListBox _teamList;
         public ListBox TeamList() => _teamList;
-        private Label _teamName;
-        public Label TeamName { get => _teamName; set => _teamName = value; }
-        private Dictionary<object,object> _teamsOfDropDownMenu;
+        private Dictionary<Canvas,Label> _teamsInDropDownMenu;
 
         public TeamSelector()
         {
             _teamList = new ListBox{MaxWidth= 135,MaxHeight = 80};
-
+            _teamsInDropDownMenu=new Dictionary<Canvas, Label>();
         }
-        // CLIPBOARD
-        /*
-         *      _teamSelector.Items.Clear();
-           _teamSelector.Items.Insert(0, Utility.GetKeyByValue(_confederationsImages, sender));
-           
-           for (int i = 1; i < 80; i++)
-           {
-           var confederation = "test";
-           Label label = new Label();
-           label.Content = confederation;
-           Canvas canvas = new Canvas { Width = 45, Height = 25 };
-           var rectangle = new Rectangle
-           {
-           Width = 40,
-           Height = 20,
-           Stroke = Brushes.Black,
-           };
-           Canvas.SetTop(rectangle, 3);
-           Canvas.SetLeft(rectangle, -1);
-           Canvas.SetLeft(label, Canvas.GetLeft(rectangle) + 40);
-           canvas.Children.Add(rectangle);
-           canvas.Children.Add(label);
-           _teamSelector.Items.Insert(i, canvas);
-           }
-         */
+
+        public void OnImageClick(object sender)
+        {
+            /*
+       *      _teamSelector.Items.Clear();
+         _teamSelector.Items.Insert(0, Utility.GetKeyByValue(_confederationsImages, sender));
+
+         for (int i = 1; i < 80; i++)
+         {
+         var confederation = "test";
+         Label label = new Label();
+         label.Content = confederation;
+         Canvas canvas = new Canvas { Width = 45, Height = 25 };
+         var rectangle = new Rectangle
+         {
+         Width = 40,
+         Height = 20,
+         Stroke = Brushes.Black,
+         };
+         Canvas.SetTop(rectangle, 3);
+         Canvas.SetLeft(rectangle, -1);
+         Canvas.SetLeft(label, Canvas.GetLeft(rectangle) + 40);
+         canvas.Children.Add(rectangle);
+         canvas.Children.Add(label);
+         _teamSelector.Items.Insert(i, canvas);
+         }
+       */
+        }
+        
+
     }
 }
