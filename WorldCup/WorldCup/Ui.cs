21d1e0963a1fd71b461c58eb4d4d08acf5bb9533
Maftoul Omar <maftoul.omar@gmail.com>
2019-02-03 22:44:21 +0100

    commit

--- WorldCup/WorldCup/Ui.cs
+++ WorldCup/WorldCup/Ui.cs
@@ -1,7 +1,7 @@
 ﻿/*Maftoul Omar 2019*/
 
 using System;
-using System.Collections.Generic
+using System.Collections.Generic;
 using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Input;
