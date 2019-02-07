534f71b04d681674fa612efb471d4a6cc568f98d
Maftoul Omar <maftoul.omar@gmail.com>
2019-02-07 22:40:30 +0100

    commit

--- WorldCup/WorldCup/Ui.cs
+++ WorldCup/WorldCup/Ui.cs
@@ -3,7 +3,7 @@
 using System;
 using System.Collections.Generic;
 using System.Windows;
-using System.Windows.Controls
+using System.Windows.Controls;
 using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Effects;
