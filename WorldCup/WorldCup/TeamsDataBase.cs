20cbc6d93a4aa9a0526b9cbe479056025e4e33b3
Maftoul Omar <maftoul.omar@gmail.com>
2019-02-11 19:59:14 +0100

    database second commit

--- WorldCup/WorldCup/TeamsDataBase.cs
+++ WorldCup/WorldCup/TeamsDataBase.cs
@@ -1,9 +1,17 @@
 ﻿/*Maftoul Omar 2019*/
+
+using System.Data.SQLite;
+
+
 namespace WorldCup
 {
         public class TeamsDataBase
         {
-            
+            private readonly string dataSource = ""
+            public void Connect()
+            {
+          
+        }
         }
 
 }
