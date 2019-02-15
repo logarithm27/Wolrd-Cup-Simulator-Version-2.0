ee07c4be082a7cd688129d5358a6b39b4196e31c
Maftoul Omar <maftoul.omar@gmail.com>
2019-02-15 22:18:35 +0100

    implementing ITeam's methods

--- WorldCup/WorldCup/Team.cs
+++ WorldCup/WorldCup/Team.cs
@@ -1,6 +1,22 @@
-﻿namespace WorldCup
+﻿using System.Collections.Generic;
+
+namespace WorldCup
 {
-    public class Team
+    public class Team : ITeam
     {
+        public string GetConfederation(Team team)
+        {
+
+        }
+
+        public int GetMaxAcceptedTeamsByConfederation(Team team, List<List<int>> pots)
+        {
+
+        }
+
+        public bool TeamIsAllowed(Team team)
+        {
+
+        }
     }
 }
