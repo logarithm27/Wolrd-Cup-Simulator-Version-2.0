18357ca5252becd6b625305df9414fcc37df4fa4
Maftoul Omar <maftoul.omar@gmail.com>
2019-02-15 22:22:20 +0100

    commit

--- WorldCup/WorldCup/Team.cs
+++ WorldCup/WorldCup/Team.cs
@@ -4,6 +4,13 @@ namespace WorldCup
 {
     public class Team : ITeam
     {
+        private string _teamName;
+        public string TeamName() => _teamName;
+        private string _team_s_confederation;
+        public string TeamSConfederation() => _team_s_confederation;
+
+        private string _teamFlag;
+        public string TeamFlag() => _teamFlag;
         public string GetConfederation(Team team)
         {
 
