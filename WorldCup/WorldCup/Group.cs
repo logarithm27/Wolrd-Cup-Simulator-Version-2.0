e8fe0e40780310f89c65e6c62fc0ce91b192d8a7
Maftoul Omar <maftoul.omar@gmail.com>
2019-02-17 23:05:30 +0100

    Implement IGroup

--- WorldCup/WorldCup/Group.cs
+++ WorldCup/WorldCup/Group.cs
@@ -1,7 +1,17 @@
-﻿namespace WorldCup
+﻿using System.Collections.Generic;
+
+namespace WorldCup
 {
     public class Group : IGroup
     {
+        public List<List<int>> GetGroups()
+        {
+            throw new System.NotImplementedException();
+        }
 
+        public bool CheckConstraint(List<Team> teams)
+        {
+            throw new System.NotImplementedException();
+        }
     }
 }
